3028dbc79a2d38e6134381a38c1ce080ec3c8284
Logan <logan.rose0329@gmail.com>
2022-02-12 14:49:25 -0500

    Finished Scraper

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Thus I have set out to build a jeopardy game that fetches a random assortment of
 
 ## Step 1: Getting the clues
 
-The first task in this project is getting the clues. Luckily, there is the fan-run [J Archive](https://j-archive.com/) that has text versions of every Jeopardy game since 1985. Unfortunately, [the site was created before REST was popular](https://www.reddit.com/r/Jeopardy/comments/5n6bw3/comment/dc93yov/?utm_source=share&utm_medium=web2x&context=3), and for the sake of data integrity, its developers will not be updating the architecture of the site. Not to be dissuaded, I decided to use BeautifulSoup to scrape the contents of the site and store it locally to be used in the web app.
+The first task in this project is getting the clues. Luckily, there is the fan-run [J Archive](https://j-archive.com/) that has text versions of every Jeopardy game since 1985. I was hoping there would be an associated REST API I could use, however, [the site was created before REST was popular](https://www.reddit.com/r/Jeopardy/comments/5n6bw3/comment/dc93yov/?utm_source=share&utm_medium=web2x&context=3), and for the sake of data integrity, its developers will not be updating the architecture of the site. Not to be dissuaded, I decided to use BeautifulSoup to scrape the contents of the site and store it locally to be used in the web app. All in all, there are nearly half a million questions. To generate the cluse.csv file, execute the main.py pyton script.
 
 ## Step 2: Making the Game
 
--- main.py
+++ main.py
@@ -2,54 +2,68 @@ from bs4 import BeautifulSoup
 from numpy import double, single
 import requests
 import re
-
-
+import csv
+import os
+import glob
+import pandas as pd
 class Clue:
-  def __init__(self, clue, answer, value):
-    self.clue = clue
+  def __init__(self, text, answer,row, col, half):
+    self.half = half
+    self.row = row
+    self.col = col
+    self.text = text
     self.answer = answer
-    self.value = value
   def getClue(self):
     return self.clue
-class Scraper:
-  def __init__(self):
-    self.html_text = requests.get('https://www.j-archive.com/showgame.php?game_id=7262').text
+class Game:
+  def __init__(self, id):
+    self.html_text = requests.get(f'https://www.j-archive.com/showgame.php?game_id={id}').text
     self.soup = BeautifulSoup(self.html_text, 'lxml')
 
+  def isValid(self):
+    error = self.soup.find('p', {"class":"error"})
+    if error:
+      return not (error.text == "ERROR: No game requested." or bool(re.match('^ERROR: No game -?[0-9]+ in database\.$',error.text)))
+    # else:
+    #   if len(self.getCategories) < 13:
+    #     return False
+    return True
+
   def getCategories(self):
-    category_names = self.soup.find_all("td", {"class":"category_name"})
-    for i in range(len(category_names)):
-      category_names[i] = category_names[i].text
-    return category_names
+    self.categoryNames = self.soup.find_all("td", {"class":"category_name"})
+    for i in range(len(self.categoryNames)):
+      self.categoryNames[i] = self.categoryNames[i].text
+    return self.categoryNames
 
-  def scrapeById(self):
+  def scrape(self):
     clues = self.soup.find_all("td", {"class":"clue"})
-    game = self.buildGame(self.getCategories())
-    singleJeopardy = game[0]
-    doubleJeopardy = game[1]
-    finalJeopardy = game[2]
+    categories = self.getCategories()
+    if categories:
+      self.game = self.buildGame(categories)
 
-    for clue in clues:
-      clueText = str(clue.find("td", {"class":"clue_text"}))
-      clueInfo = re.match('^.*<td class="clue_text" id="clue_(DJ|J)_([1-6])_([1-5])">(.*)<\/td>.*$', clueText)
-
-      if clueInfo:
-        answer =  re.match('^.*<em class=\\\\?"correct_response\\\\?">(.*)<\/em>.*$', clue.find("div", onmouseover=True, onmouseout=True, onclick=True)['onmouseover']).group(1)
-        half = clueInfo.group(1)
-        row =  int(clueInfo.group(2))
-        col =  int(clueInfo.group(3))
-        text = clueInfo.group(4)
-
-        print('\n',half,'\n',row,'\n',col,'\n',text,'\n',answer)
-
-        if half == 'J':
-          print(col-1)
-          print(row)
-          singleJeopardy[col][row-1] = Clue(text, answer, row * 200)
-        elif half == 'DJ':
-          doubleJeopardy[col][row-1] = Clue(text, answer, row * 400)
-      else: 
-        print(clue)
+      for clue in clues:
+        clueText = str(clue.find("td", {"class":"clue_text"}))
+        clueInfo = re.match('^.*<td class="clue_text" id="clue_(DJ|J)_([1-6])_([1-5])">(.*)<\/td>.*$', clueText)
+        if clueInfo:
+          answer =  re.match('^.*<em class=\\\\?"correct_response\\\\?">(.*)<\/em>.*$', clue.find("div", onmouseover=True, onmouseout=True, onclick=True)['onmouseover']).group(1)
+          half = clueInfo.group(1)
+          row =  int(clueInfo.group(3))
+          col =  int(clueInfo.group(2))
+          text = clueInfo.group(4)
+          #print('\n',half,'\n',row,'\n',col,'\n',text,'\n',answer)
+          if half == 'J':
+            self.game[0][col-1][row] = Clue(text, answer,row, col, half)
+            writer.writerow([self.categoryNames[col-1],row*200,text,answer]);
+          elif half == 'DJ':
+            self.game[1][col-1][row] = Clue(text, answer,row, col, half)
+            writer.writerow([self.categoryNames[col+5],row*400,text,answer]);
+          else:
+            self.game[2] = [Clue(text, answer,row, col,'F')]
+        # else: 
+        #   print('----')
+        #   print(clue)
+        #   print('----')
+      return self.game
 
   def displayBoard(self, board):
     for i in board:
@@ -57,32 +71,51 @@ class Scraper:
 
 
   def buildGame(self, categories):
-    singleJeopardy = [
-      ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6'  ],
-      ['200','200','200','200','200','200',],
-      ['400','400','400','400','400','400',],
-      ['600','600','600','600','600','600',],
-      ['800','800','800','800','800','800',],
-      ['1000','1000','1000','1000','1000','1000',]
+    self.singleJeopardy = [
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
     ]
     for i in range(6):
-      singleJeopardy[0][i] = categories[i]
-    doubleJeopardy = [
-      ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6'  ],
-      ['400','400','400','400','400','400',],
-      ['800','800','800','800','800','800',],
-      ['1200','1200','1200','1200','1200','1200',],
-      ['1600','1600','1600','1600','1600','1600',],
-      ['2000','2000','2000','2000','2000','2000',]
+      self.singleJeopardy[i][0] = categories[i]
+    self.doubleJeopardy = [
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
+      ['category', 'clue1', 'clue2', 'clue3', 'clue4', 'clue5'  ],
     ]
     for i in range(6, len(categories)-1, 1):
-      doubleJeopardy[0][i%6] = categories[i]
-    finalJeopardy = categories[len(categories)-1]
-    return [singleJeopardy, doubleJeopardy, finalJeopardy]
+      self.doubleJeopardy[i%6][0] = categories[i]
+    self.finalJeopardy = [categories[len(categories)-1], "clue"]
+    return [self.singleJeopardy, self.doubleJeopardy, self.finalJeopardy]
 
+f = open('clues.csv', 'w')
+writer = csv.writer(f)
+writer.writerow(['Category','Value','Clue','Answer']);
 
-scraper = Scraper()
-scraper.scrapeById()
-categories = scraper.getCategories()
-scraper.buildGame(categories)
-#scraper.getInfo()
+for i in range(7274):
+  #6224-6232 - Tournament of champions
+  #3576
+  if i not in [3576, 6224, 6225, 6226, 6227,6228,6229,6230,6231,6232]:
+    print("Getting Game" , i)
+    bucket = i // 200
+    if bucket != (i-1)//200:
+      f.close()
+      fileName = f'./data/clues_{bucket*200}-{min((bucket+1)*200-1,7274)}.csv'
+      f = open(fileName, 'x')
+      writer = csv.writer(f)
+      writer.writerow(['Category','Value','Clue','Answer']);
+    game = Game(i)
+    if game.isValid():
+      game.scrape()
+
+all_files = [i for i in glob.glob('./data/*.{}'.format('csv'))]
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_files ])
+#export to csv
+combined_csv.to_csv( "clues.csv", index=False, encoding='utf-8-sig')
